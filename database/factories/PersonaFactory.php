a85822bc3b4c02bcb1f9ce7ae00e5fe3092e652c
Nico Michelson <nicomichelson@gmail.com>
2020-04-19 23:33:43 +0000

    factory and seeder de personas

---
+++ database/factories/PersonaFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Persona;
+use Faker\Generator as Faker;
+
+$factory->define(Persona::class, function (Faker $faker) {
+    return [
+        'firstName' => $faker->name,
+        'lastName' => $faker->lastName,
+    ];
+});
---
+++ database/seeds/PersonasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PersonasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Persona::class, 20)->create();
+    }
+}
